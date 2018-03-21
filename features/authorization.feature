c830ce1dd7eb63885e70f701466e5842eab886b9
Konstantin Govorukha <kgovorukha@gmail.com>
2018-03-21 13:30:23 +0200

    Created scenarios to check /oauth/v2/token service when incorrect credentials were passed.

--- features/authorization.feature
+++ features/authorization.feature
@@ -2,7 +2,32 @@ Feature: Authorization
 
   Scenario: Send valid request to '/oauth/v2/token' and get valid response
 #    Given test step
-    When RESTClient send valid request to /oauth/v2/token
+    When RESTClient send request to /oauth/v2/token using "valid_request_to_oauth_v2_token" dataset
     Then HTTP response error message should be empty
     And  Returned HTTP response should be valid JSON
     And  Returned HTTP response should match "valid_oauth_v2_token_response_json_schema" JSON schema
+
+#  Scenario: Send invalid request to '/oauth/v2/token': incorrect 'client_secret'
+#    When RESTClient send request to /oauth/v2/token using "invalid_request_to_oauth_v2_token_with_incorrect_client_secret" dataset
+#    Then HTTP response error message should be empty
+#    And  Returned HTTP response should be valid JSON
+#    And  Returned HTTP response should match "valid_oauth_v2_token_response_json_schema" JSON schema
+#
+#  Scenario: Send invalid request to '/oauth/v2/token': incorrect 'client_id'
+#    When RESTClient send request to /oauth/v2/token using "invalid_request_to_oauth_v2_token_with_incorrect_client_id" dataset
+#    Then HTTP response error message should be empty
+#    And  Returned HTTP response should be valid JSON
+#    And  Returned HTTP response should match "valid_oauth_v2_token_response_json_schema" JSON schema
+
+
+  Scenario Outline: Send invalid request to '/oauth/v2/token': incorrect credentials
+    When RESTClient send request to /oauth/v2/token using "<credentials_dataset>" dataset
+    Then HTTP response error message should be empty
+    And  Returned HTTP response should be valid JSON
+    And  Returned HTTP response should match "<matching_json_schema>" JSON schema
+  Examples:
+    | credentials_dataset                                            | matching_json_schema                      |
+    | invalid_request_to_oauth_v2_token_with_incorrect_client_id     | error_oauth_v2_token_response_invalid_client_json_schema |
+    | invalid_request_to_oauth_v2_token_with_incorrect_client_secret | error_oauth_v2_token_response_invalid_client_json_schema |
+
+
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,8 +1,8 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+//use Behat\Gherkin\Node\PyStringNode;
+//use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Tester\Exception\PendingException;
 
 use Webmozart\Assert\Assert;
@@ -19,13 +19,6 @@ class FeatureContext implements Context
     protected $returnedHttpErrorMessage = null;
 
     const BASE_URL = 'https://sandbox.payever.de';
-
-    const VALID_AUTHENTICATION_PARAMS = array(
-        'client_id' => '30801_44jezpd83a68wc4k8c8wsssco4k0w0gow4owswoc0g0oksc8o8',
-        'client_secret' => '9vejpbzp8k08sk0k08cg00ocsgco80ckog8s800kgwcckwss0',
-        'grant_type' => 'http://www.payever.de/api/payment',
-        'scope' => 'API_CREATE_PAYMENT',
-    );
 
     /**
      * Initializes context.
@@ -48,9 +41,10 @@ class FeatureContext implements Context
     }
 
     /**
-     * @When RESTClient send valid request to \/oauth\/v2\/token
+     * @When /^RESTClient send request to \/oauth\/v2\/token using "([^"]*)" dataset$/
+     * @param $datasetName
      */
-    public function restClientSendValidRequestToOauthV2Token()
+    public function restClientSendRequestToOauthV2TokenUsingDataset($datasetName)
     {
         $curl = curl_init();
         $related_path = "/oauth/v2/token";
@@ -58,12 +52,13 @@ class FeatureContext implements Context
             CURLOPT_URL => self::BASE_URL . $related_path,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_POSTFIELDS => self::VALID_AUTHENTICATION_PARAMS,
+            CURLOPT_POSTFIELDS => $this->getDataset($datasetName),
         );
         curl_setopt_array($curl, $curl_options);
         $this->returnedHttpResponse = curl_exec($curl);
         $this->returnedHttpErrorMessage = curl_error($curl);
         curl_close($curl);
+
     }
 
     /**
@@ -98,6 +93,8 @@ class FeatureContext implements Context
      * TODO Should be moved from FeatureContext class
      *
      * @param $jsonSchema
+     * @return array
+     * @throws InvalidArgumentException
      */
     protected function jsonSchemaRepository($jsonSchemaName)
     {
@@ -118,12 +115,67 @@ class FeatureContext implements Context
                     ]
                 ];
                 break;
+            case 'error_oauth_v2_token_response_invalid_client_json_schema':
+                $returnedSchema = [
+                    'type' => 'object',
+                    'required' => ['error', 'error_description'],
+                    'properties' => [
+                        'error' => ['type' => 'string'],
+                        'error_description'   => ['type' => 'string'],
+                    ]
+                ];
+                break;
             default:
                throw new InvalidArgumentException("Unknown jsonSchemaName was passed: $jsonSchemaName");
-
         }
         return $returnedSchema;
+    }
 
+    /**
+     * Returns associated array by specified $datasetName
+     *
+     * @param $datasetName
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    protected function getDataset($datasetName)
+    {
+        $returnedDataset = null;
+
+        switch ($datasetName)
+        {
+            case 'valid_request_to_oauth_v2_token':
+                $returnedDataset = array(
+                    'client_id' => '30801_44jezpd83a68wc4k8c8wsssco4k0w0gow4owswoc0g0oksc8o8',
+                    'client_secret' => '9vejpbzp8k08sk0k08cg00ocsgco80ckog8s800kgwcckwss0',
+                    'grant_type' => 'http://www.payever.de/api/payment',
+                    'scope' => 'API_CREATE_PAYMENT',
+                );
+                break;
+            case 'invalid_request_to_oauth_v2_token_with_incorrect_client_secret':
+                $returnedDataset = array(
+                    'client_id' => '30801_44jezpd83a68wc4k8c8wsssco4k0w0gow4owswoc0g0oksc8o8',
+                    'client_secret' => 'incorrect_client_secret',
+                    'grant_type' => 'http://www.payever.de/api/payment',
+                    'scope' => 'API_CREATE_PAYMENT',
+                );
+
+                break;
+            case 'invalid_request_to_oauth_v2_token_with_incorrect_client_id':
+                $returnedDataset = array(
+                    'client_id' => 'incorrect_client_secret',
+                    'client_secret' => '9vejpbzp8k08sk0k08cg00ocsgco80ckog8s800kgwcckwss0',
+                    'grant_type' => 'http://www.payever.de/api/payment',
+                    'scope' => 'API_CREATE_PAYMENT',
+                );
+
+                break;
+
+
+            default:
+                throw new InvalidArgumentException("Unknown datasetName was passed: $datasetName");
+        }
+        return $returnedDataset;
     }
 
 }
